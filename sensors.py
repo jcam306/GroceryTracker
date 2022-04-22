57256da0a66467119b3993c30a2dab458fe83356
Jacob Camarillo <jcamarillo@ucsd.edu>
2022-04-22 13:47:29 -0700

    included create new directory

--- sensors.py
+++ sensors.py
@@ -2,22 +2,38 @@ import time
 import board
 import digitalio
 import cv2
+import os
+from datetime import datetime
 
 # set up door sensor
-door_sensor = digitalio.DigitalInOut(board.D23)
-door_sensor.direction = digitalio.Direction.INPUT
-door_sensor.pull = digitalio.Pull.UP
+doorSensor = digitalio.DigitalInOut(board.D23)
+doorSensor.direction = digitalio.Direction.INPUT
+doorSensor.pull = digitalio.Pull.UP
 
 camera = cv2.VideoCapture(0)    # Initialize the camera module
 
+version = 0
+
+# Getting the current date and time
+dt = datetime.now()
+# getting the timestamp
+# ts = datetime.timestamp(dt)
+print(str(dt))
+
+exten = os.path.join("/home/shamispi/Desktop/ece-140a-winter-2022-jcam306","tempPictures", str(dt))
+
+if not os.path.exists(exten):
+    os.mkdir(exten)
+
 while True:
 
-	if door_sensor.value:
-		print("Sensor OFF!")
-	elif (0 == door_sensor.value):
-		print("Sensor ON!")
+	if doorSensor.value:
+		print("Door CLOSED!")
+	elif (0 == doorSensor.value):
+		print("DOOR OPEN!")
 		ret, image = camera.read() 
-		cv2.imwrite(("./sensorTest.jpg"), image) 
+		cv2.imwrite(("./tempPictures/" + str(dt) +"/sensorTest" + str(version) + ".jpg"), image) 
+		version += 1
 	else:
 		print("Sensor NOT DETECTED")
 		camera.release() 
