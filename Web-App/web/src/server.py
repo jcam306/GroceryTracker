b58b3bff86e04deffa52e89b1b7ca586f3751f76
MaxGh <mghesqui@ucsd.edu>
2022-04-28 10:55:29 -0700

    added to server

--- Web-App/web/src/server.py
+++ Web-App/web/src/server.py
@@ -6,12 +6,21 @@ import fileTransfer
 def get_home(req):
   return FileResponse("home.html")
 
+def receive_file(req):
+    fileTransfer.receive()
+    #decompress file
+    #pass to classifier
+    #store results in db
+
 ''' Route Configurations '''
 if __name__ == '__main__':
   config = Configurator()
 
   config.add_route('get_home', '/')
   config.add_view(get_home, route_name='get_home')
+
+  config.add_route('file_transfer', '/transfer')
+  config.add_view(receive_file,route_name = 'file_transfer')
 
   config.add_static_view(name='/', path='./public', cache_max_age=3600)
 
