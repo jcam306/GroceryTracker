a7f1b668bbd149aaaab78798450f96911752bf77
Jacob Camarillo <jcamarillo@ucsd.edu>
2022-05-06 13:28:07 -0700

    update sender.py/sensors.py

--- sensors.py
+++ sensors.py
@@ -4,6 +4,7 @@ import digitalio
 import cv2
 import os
 from datetime import datetime
+import sender
 
 # set up door sensor
 doorSensor = digitalio.DigitalInOut(board.D23)
@@ -13,14 +14,18 @@ doorSensor.pull = digitalio.Pull.UP
 camera = cv2.VideoCapture(0)    # Initialize the camera module
 
 version = 0
+newImages = 0	# Flag for if new images were taken (So "door closed" loop doesn't endlessly try to clear image folder)
+newBatch = 0 	# New folder/batch of images
 
 # Getting the current date and time
 dt = datetime.now()
+
 # getting the timestamp
 # ts = datetime.timestamp(dt)
-print(str(dt))
+# print(str(dt))
 
-exten = os.path.join("/home/shamispi/Desktop/ece-140a-winter-2022-jcam306","tempPictures", str(dt))
+# exten = os.path.join("/home/shamispi/Desktop/ece-140a-winter-2022-jcam306","tempPictures", str(dt))
+exten = os.path.join("/home/shamispi/Desktop/","images")
 
 if not os.path.exists(exten):
     os.mkdir(exten)
@@ -28,12 +33,24 @@ if not os.path.exists(exten):
 while True:
 
 	if doorSensor.value:
-		print("Door CLOSED!")
-	elif (0 == doorSensor.value):
-		print("DOOR OPEN!")
+		print("Door OPEN!")
+		if newBatch == 1:		# Reset Date Time if new batch of images
+			newBatch = 0
+			dt = datetime.now()
 		ret, image = camera.read() 
-		cv2.imwrite(("./tempPictures/" + str(dt) +"/sensorTest" + str(version) + ".jpg"), image) 
+		# cv2.imwrite(("./tempPictures/" + str(dt) +"/sensorTest" + str(version) + ".jpg"), image) 
+		cv2.imwrite(("./images/" + str(dt) + "_" + str(version) + ".jpg"), image) 
 		version += 1
+		newImages = 1
+	elif (0 == doorSensor.value):
+		print("DOOR CLOSED!")
+		if newImages == 1:
+			#call sender.py and send images
+			sender.sendFolder(exten)
+			for file in os.listdir(exten):				# Clear folder on images from previous image capture sequence
+				os.remove(os.path.join(exten, file))
+			newImages = 0								# Reset if new images to send
+			newBatch = 1								# Reset for new batch of images
 	else:
 		print("Sensor NOT DETECTED")
 		camera.release() 
