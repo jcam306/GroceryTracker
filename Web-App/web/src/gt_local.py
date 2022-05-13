4c7212fbb3934a7197e3edee029032ba5ae9b466
Jacob Camarillo <jcamarillo@ucsd.edu>
2022-05-13 13:39:25 -0700

    update gt_local.py

--- Web-App/web/src/gt_local.py
+++ Web-App/web/src/gt_local.py
@@ -13,7 +13,8 @@ PAT = '9f8278ad4e7a4549a7b1f43b9e538ec0'
 APP_ID = 'Grocery-Tracker'
 MODEL_ID = 'aaa03c23b3724a16a56b629203edc62c'
 # Change this to whatever image URL you want to process
-folder_loc = r'C:\Users\ihyun\Desktop\GroceryTracker\Web-App\web\src\public\images'
+folder_loc = r'C:\Users\jcam3\GitHub Repositories\ECE140B\GroupProject\GroceryTracker\Web-App\web\src\public\images'
+folder_loc2 = r'C:\Users\jcam3\GitHub Repositories\ECE140B\GroupProject\GroceryTracker\Web-App\web\src\processed_images'
 # This is optional. You can specify a model version or the empty string for the default
 MODEL_VERSION_ID = ''
 
@@ -21,7 +22,8 @@ MODEL_VERSION_ID = ''
 # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
 ############################################################################
 
-def img_pro(img):
+def img_pro(img, name):
+    print(img)
     image = cv.imread(img, 1)
     gray = cv.cvtColor(image.copy(), cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray, (11, 11), 0)
@@ -35,14 +37,11 @@ def img_pro(img):
     first_contour = cv.drawContours(image.copy(), biggest, -1,(255,0,255),3)
     x, y, w, h = cv.boundingRect(biggest)
     cv.rectangle(first_contour,(x,y), (x+w,y+h), (255,0,0), 5)
-    cv.imshow('First contour with bounding box', first_contour)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    print('Total number of contours detected: ' + str(len(contours)))
-    
 
     result = image[y:y+h, x:x+w]
-    cv.imwrite("Result.png", result)
+    # cv.imwrite("Result.png", result)
+    file_path = os.path.join(folder_loc2, name)
+    cv.imwrite(file_path, result)
 
 def tracking(img):
     channel = ClarifaiChannel.get_grpc_channel()
@@ -88,7 +87,9 @@ def tracking(img):
     #   print("%s %.2f" % (concept.name, concept.value))
 if __name__ == "__main__":
     for img in os.listdir(folder_loc):
-        img2 = os.path.join(folder_loc, "milk.jpg")
-        img_pro(img2)
+        if img == "received":
+            continue
+        img2 = os.path.join(folder_loc, img)
+        img_pro(img2, img)
         #data = tracking(img2)
         #print("%s %.3f" % (data.name, data.value))
