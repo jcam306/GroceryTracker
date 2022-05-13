a3977ec37123e66cec70eeeb97efdf9f5f3ea8ba
Jacob Camarillo <jcamarillo@ucsd.edu>
2022-05-13 13:03:11 -0700

    update file name

--- sender.py
+++ sender.py
@@ -7,7 +7,7 @@ def sendFolder(folderPath):
 
     for file in os.listdir(folderPath):				# Clear folder on images from previous image capture sequence
         #('images', ("imageName.jpg",open(folderPath + file, 'rb'), 'image/jpg'))
-        newFile = ('images', (file,open(folderPath + "/" + file, 'rb'), 'image/jpg'))
+        newFile = (file, (file,open(folderPath + "/" + file, 'rb'), 'image/jpg'))
         multiple_files.append(newFile)
 
     print(multiple_files)
