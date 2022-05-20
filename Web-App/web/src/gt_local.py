f64213370b357bfc41928276205a13f7d44ad7f0
SikGek <81644416+SikGek@users.noreply.github.com>
2022-05-20 06:55:15 -0700

    stuph added

--- Web-App/web/src/gt_local.py
+++ Web-App/web/src/gt_local.py
@@ -5,25 +5,24 @@ import os
 from os import listdir
 import cv2 as cv
 import numpy as np
-
+#food-item-v1-recognition
+#food-item-recognition
 
 USER_ID = 'sikgaek'
-# Your PAT (Personal Access Token) can be found in the portal under Authentification
+
 PAT = '9f8278ad4e7a4549a7b1f43b9e538ec0'
 APP_ID = 'Grocery-Tracker'
-MODEL_ID = 'aaa03c23b3724a16a56b629203edc62c'
-# Change this to whatever image URL you want to process
-folder_loc = r'C:\Users\jcam3\GitHub Repositories\ECE140B\GroupProject\GroceryTracker\Web-App\web\src\public\images'
-folder_loc2 = r'C:\Users\jcam3\GitHub Repositories\ECE140B\GroupProject\GroceryTracker\Web-App\web\src\processed_images'
-# This is optional. You can specify a model version or the empty string for the default
+MODEL_ID = 'food-item-v1-recognition'
+
+#folder_loc = r'C:\Users\jcam3\GitHub Repositories\ECE140B\GroupProject\GroceryTracker\Web-App\web\src\public\images'
+#folder_loc2 = r'C:\Users\jcam3\GitHub Repositories\ECE140B\GroupProject\GroceryTracker\Web-App\web\src\processed_images'
+folder_loc = r'C:\Users\ihyun\Desktop\GroceryTracker\Web-App\web\src\public\images'
+folder_loc2 = r'C:\Users\ihyun\Desktop\GroceryTracker\Web-App\web\src\public\processed_images'
+
 MODEL_VERSION_ID = ''
 
-############################################################################
-# YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-############################################################################
 
 def img_pro(img, name):
-    print(img)
     image = cv.imread(img, 1)
     gray = cv.cvtColor(image.copy(), cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray, (11, 11), 0)
@@ -37,16 +36,16 @@ def img_pro(img, name):
     first_contour = cv.drawContours(image.copy(), biggest, -1,(255,0,255),3)
     x, y, w, h = cv.boundingRect(biggest)
     cv.rectangle(first_contour,(x,y), (x+w,y+h), (255,0,0), 5)
-    cv.imshow('First contour with bounding box', first_contour)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    print('Total number of contours detected: ' + str(len(contours)))
-
-
+    #cv.imshow('First contour with bounding box', first_contour)
+    #cv.waitKey(0)
+    #cv.destroyAllWindows()
+    #print('Total number of contours detected: ' + str(len(contours)))
     result = image[y:y+h, x:x+w]
     # cv.imwrite("Result.png", result)
     file_path = os.path.join(folder_loc2, name)
+    #print(file_path)
     cv.imwrite(file_path, result)
+    return x
 
 def tracking(img):
     channel = ClarifaiChannel.get_grpc_channel()
@@ -83,18 +82,48 @@ def tracking(img):
     # Since we have one input, one output will exist here
     output = post_model_outputs_response.outputs[0]
 
-    return output.data.concepts
+    return output.data.concepts[0]
     #print("Outcome:")
     #print("%s %.3f" % (output.data.concepts[0].name, output.data.concepts[0].value))
 
 
     #for concept in output.data.concepts:
     #   print("%s %.2f" % (concept.name, concept.value))
+
+def dup_img(x):
+    prev = []
+    for img in os.listdir(folder_loc2):
+        img2 = os.path.join(folder_loc2, img)
+        image = cv.imread(img2, 1)
+        #print(np.size(prev))
+        if np.size(prev) == 0:
+            prev = image
+            continue
+        image = np.resize(image, (np.shape(prev)))
+        avg = np.average(image - prev)
+        #print(avg, img2)
+        if avg < 30:
+            os.remove(img2)
+            '''if x - x2 > 0:
+                mov = "OUT"
+            else:
+                mov = "IN"
+            return mov'''
+            prev = image
+        prev = image
+        x2 = x
+
 if __name__ == "__main__":
     for img in os.listdir(folder_loc):
         if img == "received":
             continue
         img2 = os.path.join(folder_loc, img)
-        img_pro(img2, img)
-        #data = tracking(img2)
-        #print("%s %.3f" % (data.name, data.value))
+        x = img_pro(img2, img)
+        dup_img(x)
+        img3 = os.path.join(folder_loc2, img)
+        #print(img3)
+        try:
+            data = tracking(img3)
+        except:
+            continue
+        print("%s %.3f" % (data.name, data.value))
